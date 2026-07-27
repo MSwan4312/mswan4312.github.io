7c667968509b0c65e99ae40e298f5803fdbd72d5
MSwan4312 <117595402+MSwan4312@users.noreply.github.com>
2026-07-27 16:35:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+	const options = {
+		timeZone: 'America/Denver',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+		hour12: true
+	};
+	const formatter = new Intl.DateTimeFormat('en-US', options);
+	document.getElementById('currentTime').innerHTML = formatter.format(new Date()) + ' MST';
 }
 showTime();
 setInterval(function () {
