48a52b95d211ffcd202d8196f4390b31623c9e17
MSwan4312 <117595402+MSwan4312@users.noreply.github.com>
2026-07-27 16:44:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function showTime() {
 	const options = {
 		timeZone: 'America/Denver',
+		weekday: 'short',
+		month: 'short',
+		day: 'numeric',
+		year: 'numeric',
 		hour: '2-digit',
 		minute: '2-digit',
 		second: '2-digit',
